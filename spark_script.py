8e101050301e24930db4c4108cf6c5a2afaebfe8
Jasmine <marwahaj@uci.edu>
2026-06-26 17:07:42 -0700

    integrated csv file into postgres, loaded into spark, manipulated postgres data

--- spark_script.py
+++ spark_script.py
@@ -9,32 +9,31 @@ import pyspark.sql.functions as F
 
 spark = SparkSession.builder\
         .appName("PostgresToBigQuery")\
+        .config("spark.jars", "/Users/jasminemarwaha/Documents/ETL_pipeline/postgresql-42.7.3.jar") \
         .getOrCreate()
 
-data_csv = (
+# data_csv = (
+#     spark
+#     .read
+#     .format("csv")
+#     .option("header", "true")
+#     .option("inferSchema", "true")
+#     .load("./csv_files/*.csv")
+# )
+df = (
     spark
     .read
-    .format("csv")
-    .option("header", "true")
-    .option("inferSchema", "true")
-    .load("./csv_files/*.csv")
+    .format("jdbc")
+    .option("url", "jdbc:postgresql://localhost:5432/db123")
+    .option("dbtable", "employees")
+    .option("user", "user123")
+    .option("password", "password123")
+    .option("driver", "org.postgresql.Driver")
+    .load()
 )
-#using jdbc to read/write from databases
-# jdbc_url = "jdbc:postgresql://<YOUR_POSTGRES_HOST>:<PORT>/<DATABASE_NAME>"
-# jdbc_connection_properties = {
-#     "user": "<YOUR_USERNAME>",
-#     "password": "<YOUR_PASSWORD>",
-#     "driver": "org.postgresql.Driver"
-# }
-# #writing the local csv files to postgres
-# data_csv.write \
-#     .jdbc(url=jdbc_url, table="raw_csv_data", mode="overwrite", properties=jdbc_connection_properties)
-# #reading those files from postgres into spark
-# df_postgres = spark.read \
-#     .jdbc(url=jdbc_url, table="raw_csv_data", properties=jdbc_connection_properties)
 
 df_manipulated = (
-    data_csv.withColumn("status", F.lit("On Leave"))
+    df.withColumn("status", F.lit("On Leave"))
 )
 df_manipulated.show(10)
 
